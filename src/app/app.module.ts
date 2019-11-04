07a2fedf9be0f6c953a865bb2e6bad9d1d11f22d
JonyDMorales <jonatan_moralest@hotmail.com>
2019-11-03 20:15:39 -0600

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FooterTransparentComponent } from './components/footer-transparent/footer-transparent.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    FooterTransparentComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
